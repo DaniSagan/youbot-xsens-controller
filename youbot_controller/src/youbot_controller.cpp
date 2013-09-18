f413b8a9e364bcb74c29666094d46741b2529cca
Daniel <daniel@daniel-Lenovo-G580.(none)>
2013-09-18 12:03:10 +0200

    Improved youbot arm control with xsens IMUs

--- youbot_controller/src/youbot_controller.cpp
+++ youbot_controller/src/youbot_controller.cpp
@@ -63,7 +63,7 @@ int main(int argc, char** argv)
     
     std::vector<double> angs;
     
-    unsigned int c = 200;
+    unsigned int c = 50;
     double ang_0_offset = 0;
     ROS_INFO("Calibrating Sensor Orientation...");
     
@@ -128,6 +128,7 @@ double NormalizeAngle(double angle)
     return angle;
 }
 
+/* outdated function
 std::vector<double> GetAngles(xsens::SensorSubscriberList& sensors)
 {
     std::vector<double> angles(5);
@@ -187,6 +188,73 @@ std::vector<double> GetAngles(xsens::SensorSubscriberList& sensors)
     angles[3] = offsets[3] + 1.0 * (yaw_12);
     //angles[4] = offsets[4] ;//+ 2.0 * (-roll_01);
     angles[4] = 2.91237f;
+    
+    return angles;
+}
+*/
+
+std::vector<double> GetAngles(xsens::SensorSubscriberList& sensors)
+{
+    std::vector<double> angles(5);
+    
+    dfv::Quaternion q0 = sensors.GetOriQuat(0);
+    dfv::Quaternion q1 = sensors.GetOriQuat(1);
+    dfv::Quaternion q2 = sensors.GetOriQuat(2);
+
+    dfv::Quaternion q01 = q0.GetConjugate() * q1;
+    dfv::Quaternion q12 = q1.GetConjugate() * q2;
+    
+    // Cálculo de los ángulos entre cada sensor
+    
+    double yaw_0;
+    double pitch_0;
+    double pitch_1;
+    double pitch_2;
+    
+    dfv::Vector3 ii = dfv::Vector3::i.GetRotated(q0);
+    dfv::Vector3 jj = dfv::Vector3::j.GetRotated(q0);
+    dfv::Vector3 kk = dfv::Vector3::k.GetRotated(q0);
+    
+    if (kk.z >= 0)
+    {
+        //yaw_0 = atan2(ii.y, ii.x);
+        yaw_0 = atan2(-jj.x, jj.y);
+        pitch_0 = atan2(ii.z, sqrt(ii.x*ii.x + ii.y*ii.y));
+    }
+    else
+    {
+        yaw_0 = atan2(-jj.x, jj.y);
+        pitch_0 = atan2(ii.z, -sqrt(ii.x*ii.x + ii.y*ii.y));
+    }
+    
+    ii = dfv::Vector3::i.GetRotated(q01);
+    jj = dfv::Vector3::j.GetRotated(q01);
+    kk = dfv::Vector3::k.GetRotated(q01);
+    
+    pitch_1 = atan2(ii.z, ii.x);
+    
+    ii = dfv::Vector3::i.GetRotated(q12);
+    jj = dfv::Vector3::j.GetRotated(q12);
+    kk = dfv::Vector3::k.GetRotated(q12);
+    
+    pitch_2 = atan2(ii.z, ii.x);
+    
+    // Adaptación de los ángulos obtenidos
+    // teniendo en cuenta los offsets de cada
+    // articulación
+    
+    std::cout << "pitch_0: " << pitch_0 << std::endl;
+    std::cout << "pitch_1: " << pitch_1 << std::endl;
+    std::cout << "pitch_2: " << pitch_2 << std::endl;
+    
+    double offsets[] = {2.9597f, -0.4263f, -2.5712f, 1.8111f, 2.9124f};
+    
+    double angs[5];
+    angles[0] = offsets[0] - yaw_0;
+    angles[1] = offsets[1] + pitch_0;
+    angles[2] = offsets[2] + pitch_1;
+    angles[3] = offsets[3] + pitch_2;
+    angles[4] = offsets[4];
     
     return angles;
 }
